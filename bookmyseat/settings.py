b9398e9d3defdf9a88dd5ad43ec002c840e7b56c
Vaibhavi <114639467+Vk-543@users.noreply.github.com>
2025-04-11 13:45:52 +0530

    settings.py

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','django-project_update-1.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','django-book.vercel.app']
 
 # Application definition
 
